96356925e2e8bbcb4cd0b6f33ce34408102f0dfc
hyh <buzhiming_hao@163.com>
2022-08-09 17:35:13 +0800

    fix: home entry link;

--- src/pages/index.js
+++ src/pages/index.js
@@ -23,7 +23,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro"
+            to="/docs/web/Web入门"
           >
             全部博客 ➡️
           </Link>
